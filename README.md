f46b88c4f0e7c33707f287c17bb3fac50aee0e6f
Joe Ivans <joseph.ivans@gmail.com>
2021-08-18 07:02:31 -0700

    Implement remaining features

--- README.md
+++ README.md
@@ -16,4 +16,22 @@ Finish time: 22:44<br/>
 Actual time needed to complete: 2h10m<br/>
 ___
 
+### Feature
+
+Number and name of feature: 2 Allow users to vote for their favorite beast<br/>
+Estimate of time needed to complete: 20m<br/>
+Start time: 22:52<br/>
+Finish time: 01:15<br/>
+Actual time needed to complete: 2h23m<br/>
+___
+### Feature
+
+Number and name of feature: 3 Bootstrap<br/>
+Estimate of time needed to complete: 20m<br/>
+Start time: 06:45<br/>
+Finish time: 07:02<br/>
+Actual time needed to complete: 17m<br/>
+___
+
+
 &copy; 2021 Joe Ivans, Student, Code Fellows Seattle
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import beasts from './data.json';
 import {HornedBeasts} from './hornedBeasts';
-import './main.css';
 
 
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function Main() {
   'use strict';
 
@@ -13,16 +19,84 @@ export function Main() {
     </main>
   );
 
-  function beastMaker() {
+  /**
+   *
+   * @param {number} columns
+   * @returns {JSX.Element[]}
+   */
+  function beastMaker(columns = 3) {
+    /*
     const result = [];
 
-    beasts.forEach(beast => {
+    for (let i = 0; i < beasts.length; i += columns) {
+      let row = document.createElement('Row');
+      for (let j = 0; j < columns; j++) {
+        try {
+          row.appendChild(<HornedBeasts description={beasts[i + j].description}
+                                        title={beasts[i + j].title}
+                                        imageUrl={beasts[i + j].image_url}
+                                        altText={beasts[i + j].keyword}/>);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      result.push(row);
+    }
+    return result;
+     */
+
+    // I'd like to know how to do this more programmatically
+    // like the above attempt, with a Row reference,
+    // appending children to it before pushing to the array
+
+    const result = [];
+
+    const remainder = beasts.length % 3;
+    console.log(remainder);
+
+    for (let i = 0; i < beasts.length - remainder; i += columns) {
       result.push(
-        <HornedBeasts description={beast.description}
-                      title={beast.title}
-                      imageUrl={beast.image_url}
-                      altText={beast.keyword}/>);
-    });
+        <Row>
+          <Col>
+            <HornedBeasts description={beasts[i + 0].description}
+                          title={beasts[i + 0].title}
+                          imageUrl={beasts[i + 0].image_url}
+                          altText={beasts[i + 0].keyword}/>
+          </Col>
+          <Col>
+            <HornedBeasts description={beasts[i + 1].description}
+                          title={beasts[i + 1].title}
+                          imageUrl={beasts[i + 1].image_url}
+                          altText={beasts[i + 1].keyword}/>
+          </Col>
+          <Col>
+            <HornedBeasts description={beasts[i + 2].description}
+                          title={beasts[i + 2].title}
+                          imageUrl={beasts[i + 2].image_url}
+                          altText={beasts[i + 2].keyword}/>
+          </Col>
+        </Row>);
+    }
+
+    // I know there's 2 left but I don't know (yet) how to
+    // make a row and append children to it cleanly
+    // while respecting the remainder
+    const i = beasts.length - 2;
+    result.push(
+      <Row>
+        <Col>
+          <HornedBeasts description={beasts[i + 0].description}
+                        title={beasts[i + 0].title}
+                        imageUrl={beasts[i + 0].image_url}
+                        altText={beasts[i + 0].keyword}/>
+        </Col>
+        <Col>
+          <HornedBeasts description={beasts[i + 1].description}
+                        title={beasts[i + 1].title}
+                        imageUrl={beasts[i + 1].image_url}
+                        altText={beasts[i + 1].keyword}/>
+        </Col>
+      </Row>);
 
     return result;
   }
