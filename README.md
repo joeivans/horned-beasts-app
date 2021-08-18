c2d5f7c18a6cc08d3db876a3801791ef20310e3e
Joe Ivans <joseph.ivans@gmail.com>
2021-08-17 22:46:35 -0700

    Add feature 1 impl

---
+++ README.md
@@ -0,0 +1,19 @@
+# Horned Beasts!
+
+- Student project
+- Pictures of animals with horns!!!
+
+## Feature Estimates
+
+___
+
+### Feature
+
+Number and name of feature: 1 Display Images<br/>
+Estimate of time needed to complete: 20m<br/>
+Start time: 19:37<br/>
+Finish time: 22:44<br/>
+Actual time needed to complete: 2h10m<br/>
+___
+
+&copy; 2021 Joe Ivans, Student, Code Fellows Seattle
--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import beasts from './data.json';
 import {HornedBeasts} from './hornedBeasts';
 import './main.css';
 
+
 export function Main() {
+  'use strict';
+
   return (
     <main>
-      <HornedBeasts description="A goat" title="Hi" imageUrl="./logo192.png" altText="It says Hi"/>
-      <HornedBeasts description="An elk" title="How are you?" imageUrl="./logo192.png" altText="It says How are you?"/>
+      {beastMaker()}
     </main>
   );
+
+  function beastMaker() {
+    const result = [];
+
+    beasts.forEach(beast => {
+      result.push(
+        <HornedBeasts description={beast.description}
+                      title={beast.title}
+                      imageUrl={beast.image_url}
+                      altText={beast.keyword}/>);
+    });
+
+    return result;
+  }
 }
